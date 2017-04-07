12d5557ca1b533050fe736b778886b898e767e20
Shurui Zhou <shuruiz@Shuruis-MBP.wv.cc.cmu.edu>
2017-04-07 17:27:51 -0400

    get diff html file, ready to hack

--- src/MocGithub/ParseHtml.java
+++ src/MocGithub/ParseHtml.java
@@ -1,17 +1,24 @@
 package MocGithub;
 
 
+import Util.JsonUtility;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
 
 /**
  * Created by shuruiz on 4/5/17.
  */
 public class ParseHtml {
+    static final String FS = File.separator;
+    static String github_api = "https://api.github.com/repos/";
+    static String github_page = "https://github.com/";
 
     public void generateMocGithubForkPage(String forkname) {
         Document doc_head, doc_body, doc_newPage;
@@ -36,7 +43,7 @@ public class ParseHtml {
             String codeChangeElement = pr_tag.toString().replaceFirst("Pull\\srequests", "Code changes");
             tags.append(codeChangeElement);
             doc_body.getElementsByClass("tabnav-tabs").get(0).getElementsByClass("tabnav-tab  js-pjax-history-navigate").get(0).remove();
-
+            doc_body.getElementsByClass("BtnGroup").remove();
 
             System.out.print("");
 
@@ -45,11 +52,40 @@ public class ParseHtml {
         }
     }
 
+    public void getDiffPage(String forkName) {
+        String forkUrl = github_api + forkName;
+        JsonUtility jsonUtility = new JsonUtility();
+        try {
+            // get latest commit sha
+            String[] todayTimeStamp = new Timestamp(System.currentTimeMillis()).toString().split(" ");
+            String format_current_time = todayTimeStamp[0] + "T" + todayTimeStamp[1].split("\\.")[0] + "Z";
+            JSONObject fork_commit_jsonObj = new JSONObject(jsonUtility.readUrl(github_api + forkName + "/commits?until=") + todayTimeStamp);
+            String latestCommitSHA = fork_commit_jsonObj.getString("sha");
+
+            //get general fork information
+            JSONObject fork_jsonObj = new JSONObject(jsonUtility.readUrl(forkUrl));
+            //get commit before fork was created
+            String firstCommitTimeStamp = fork_jsonObj.getString("created_at");
+            JSONObject upstreamInfo = (JSONObject) fork_jsonObj.get("parent");
+            String upstreamName = upstreamInfo.getString("full_name");
+            JSONObject upstream_jsonObj = new JSONObject(jsonUtility.readUrl(github_api + upstreamName + "/commits?until=" + firstCommitTimeStamp));
+            String SHA_beforeForkCreated = upstream_jsonObj.getString("sha");
+            System.out.println(firstCommitTimeStamp);
+
+            String diff_url = github_page + upstreamName + "/compare/" + SHA_beforeForkCreated + "..." + forkName.split(FS)[0] + ":" + latestCommitSHA;
+
+            System.out.print("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         String forkName = "malx122/Marlin";
 
         ParseHtml parseHtml = new ParseHtml();
-        parseHtml.generateMocGithubForkPage(forkName);
+        parseHtml.getDiffPage(forkName);
+
+//        parseHtml.generateMocGithubForkPage(forkName);
     }
 }
---
+++ src/Util/JsonUtility.java
@@ -0,0 +1,72 @@
+package Util;
+
+/**
+ * Created by shuruiz on 4/7/17.
+ */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JsonUtility {
+
+    public static String json = "{ \"id\": 01, \"language\": \"Java\", \"edition\": \"third\", \"author\": \"Herbert Schildt\", \"chapters\": [\"chapter 1\",\"chapter 2\",\"chapter 3\"] }";
+
+    public void readJson(String json) {
+        JSONObject jsonObj = new JSONObject(json);
+
+        String id = jsonObj.getString("id");
+        System.out.println(id);
+
+        String language = jsonObj.getString("language");
+        System.out.println(language);
+
+        String edition = jsonObj.getString("edition");
+        System.out.println(edition);
+
+        String author = jsonObj.getString("author");
+        System.out.println(author);
+
+        JSONArray chapters = (JSONArray) jsonObj.get("chapters");
+        Iterator<Object> iterator = chapters.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            String json_string = buffer.toString();
+            if(json_string.startsWith("[")){
+                json_string=json_string.substring(1,json_string.lastIndexOf(']')-2);
+            }
+
+            return json_string;
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
+
+
+    public static void main(String args[]) {
+        JsonUtility utility = new JsonUtility();
+        //read
+        utility.readJson(json);
+    }
+
+}
