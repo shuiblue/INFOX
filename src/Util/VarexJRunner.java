cd5cd24fe269e4da1f0e683a14ed0d548bc95001
Shurui Zhou <shuruiz@Shuruis-MBP.wv.cc.cmu.edu>
2017-06-09 15:58:42 -0400

    add varexJRunner to catch r lib  non-zero exit

---
+++ src/Util/VarexJRunner.java
@@ -0,0 +1,77 @@
+package Util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+
+public class VarexJRunner {
+	
+	static PrintStream out = System.out; 
+
+	public static void main(String[] args) {
+		List<String> commands = new LinkedList<>();
+		commands.add("./gradlew run");
+
+		process(commands);
+		out.println();
+		out.flush();
+		out.close();
+	}
+
+	private static void process(List<String> commands) {
+		ProcessBuilder processBuilder = new ProcessBuilder(commands);
+		BufferedReader input = null;
+		BufferedReader error = null;
+		try {
+			Process process = processBuilder.start();
+			input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			while (true) {
+				try {
+					String line;
+					while ((line = input.readLine()) != null) {
+						System.out.println(line);
+					}
+					while ((line = error.readLine()) != null) {
+						System.out.println(line);
+					}
+
+					try {
+						process.waitFor();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					int exitValue = process.exitValue();
+					if (exitValue != 0) {
+						throw new IOException("The process doesn't finish normally (exit=" + exitValue + ")!");
+					}
+					break;
+				} catch (IllegalThreadStateException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (input != null) {
+					input.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (error != null) {
+					try {
+						error.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
