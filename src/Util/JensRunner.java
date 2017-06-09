ca74d3caf57adebee6e4594e3dd261d86da84895
Shurui Zhou <shuruiz@Shuruis-MBP.wv.cc.cmu.edu>
2017-06-09 17:24:34 -0400

    fix bugs

--- src/Util/JensRunner.java
+++ src/Util/JensRunner.java
@@ -11,70 +11,60 @@ public class JensRunner {
 	
 	static PrintStream out = System.out; 
 
+	static boolean coreFailure = true;
+
 	public static void main(String[] args) {
 		System.out.println("Execute: ");
 		List<String> commands = new LinkedList<>();
 		commands.add("./gradlew");
 		commands.add("run");
-		System.out.println(commands);
 
-		process(commands);
-		out.println();
-		out.flush();
-		out.close();
+		while (coreFailure) {
+			coreFailure = false;
+			System.out.println(commands);
+			process(commands);
+		}
+
 	}
 
 	private static void process(List<String> commands) {
 		ProcessBuilder processBuilder = new ProcessBuilder(commands);
-		BufferedReader input = null;
-		BufferedReader error = null;
 		try {
 			Process process = processBuilder.start();
-			input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-			while (true) {
-				try {
-					String line;
-					while ((line = input.readLine()) != null) {
-						System.out.println(line);
-					}
-					while ((line = error.readLine()) != null) {
-						System.out.println(line);
-					}
-
+			try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				 BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+				while (true) {
 					try {
-						process.waitFor();
-					} catch (InterruptedException e) {
+						String line;
+						while ((line = input.readLine()) != null) {
+							System.out.println(line);
+						}
+						while ((line = error.readLine()) != null) {
+							System.out.println(line);
+						}
+
+						try {
+							process.waitFor();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						int exitValue = process.exitValue();
+						if (exitValue != 0) {
+							if (exitValue == 134) {
+								coreFailure = true;
+								return;
+							}
+
+							throw new IOException("The process doesn't finish normally (exit=" + exitValue + ")!");
+						}
+						break;
+					} catch (IllegalThreadStateException e) {
 						e.printStackTrace();
 					}
-					int exitValue = process.exitValue();
-					if (exitValue != 0) {
-						throw new IOException("The process doesn't finish normally (exit=" + exitValue + ")!");
-					}
-					break;
-				} catch (IllegalThreadStateException e) {
-					e.printStackTrace();
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (input != null) {
-					input.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				if (error != null) {
-					try {
-						error.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
 		}
 	}
 }
