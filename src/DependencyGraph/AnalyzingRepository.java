43c30aa156be57f6b203e66b4b4f9521f0b267e0
Shurui Zhou <shuruiz@SHURUIZ-NEW.WV.CC.CMU.EDU>
2017-07-11 16:46:16 -0400

    new version INFOX

--- src/DependencyGraph/AnalyzingRepository.java
+++ src/DependencyGraph/AnalyzingRepository.java
@@ -106,19 +106,19 @@ public class AnalyzingRepository {
 
 
     private void executeINFOX(String sourcecodeDir, String analysisDirName, String testCaseDir, int approachIndex, Rengine re, boolean isMS_CLUSTERCHANGES, int max_numberOfCut, boolean directedGraph, String testDir, boolean hasGroundTruth, int clusterSizeThreshold) {
-//        if (!directedGraph) {
-//            DependencyGraph dependencyGraph = new DependencyGraph(approachIndex);
-//            /**  this function extract changed_code_dependency_graph from complete graph**/
-////            dependencyGraph.generateChangedDependencyGraphFromCompleteGraph(sourcecodeDir, analysisDirName, testCaseDir, testDir, re);
-//
-//            /**  this function generate all the graph at the same time **/
-//            dependencyGraph.getDependencyGraphForProject(sourcecodeDir, testCaseDir, testDir);
-//        }
-//
-//
-//        /** Community Detection  **/
-//        R_CommunityDetection communityDetection = new R_CommunityDetection(sourcecodeDir, analysisDirName, testCaseDir, testDir, re,max_numberOfCut,numberOfBiggestClusters,  clusterSizeThreshold);
-//        communityDetection.clustering_CodeChanges(testCaseDir, testDir, re, directedGraph,true,"original");
+        if (!directedGraph) {
+            DependencyGraph dependencyGraph = new DependencyGraph(approachIndex);
+            /**  this function extract changed_code_dependency_graph from complete graph**/
+//            dependencyGraph.generateChangedDependencyGraphFromCompleteGraph(sourcecodeDir, analysisDirName, testCaseDir, testDir, re);
+
+            /**  this function generate all the graph at the same time **/
+            dependencyGraph.getDependencyGraphForProject(sourcecodeDir, testCaseDir, testDir);
+        }
+
+
+        /** Community Detection  **/
+        R_CommunityDetection communityDetection = new R_CommunityDetection(sourcecodeDir, analysisDirName, testCaseDir, testDir, re,max_numberOfCut,numberOfBiggestClusters,  clusterSizeThreshold);
+        communityDetection.clustering_CodeChanges(testCaseDir, testDir, re, directedGraph,true,"original");
 
         /** Generating html to visualize source code, set background and left side bar color for new code  **/
         AnalyzingCommunityDetectionResult analyzingCommunityDetectionResult = new AnalyzingCommunityDetectionResult(sourcecodeDir, testCaseDir, testDir, isMS_CLUSTERCHANGES, max_numberOfCut, numberOfBiggestClusters,clusterSizeThreshold);
--- src/INFOX_main.java
+++ src/INFOX_main.java
@@ -64,8 +64,8 @@ public class INFOX_main {
             System.out.println("Cannot load R");
             return;
         }
-//        for (String forkName : forkListArray) {
-        String forkName = "626Pilot/Smoothieware";
+        for (String forkName : forkListArray) {
+//        String forkName = "626Pilot/Smoothieware";
         String branchName = "";
             boolean hasGroundTruth = false;
             String testCasesDir;
@@ -76,10 +76,10 @@ public class INFOX_main {
                 testCasesDir = "/home/feature/shuruiz/INFOX_testCases/";
             }
 
-//            File dir = new File(testCasesDir + "/" + forkName);
-//            if (dir.exists()) {
-//                continue;
-//            }
+            File dir = new File(testCasesDir + "/" + forkName);
+            if (dir.exists()) {
+                continue;
+            }
 
 
             Root_Dir = new ProcessingText().getRootDir();
@@ -92,33 +92,33 @@ public class INFOX_main {
 
                 File file = new File(localSourceCodeDirPath);
 
-//                if (!file.exists()) {
-//
-//                    /***git clone repo to local dir***/
-//                    JgitUtility jgitUtility = new JgitUtility();
-//                    String uri = github_page + forkName + ".git";
-//                    System.out.println("Cloning repo from github: " + forkName + " to " + testCasesDir);
-//
-//                    jgitUtility.cloneRepo(uri, localSourceCodeDirPath, branchName);
-//                    if (forkName.contains("Marlin")) {
-//                        try {
-//                            FileUtils.deleteDirectory(new File(localSourceCodeDirPath + "ArduinoAddons"));
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
+                if (!file.exists()) {
+
+                    /***git clone repo to local dir***/
+                    JgitUtility jgitUtility = new JgitUtility();
+                    String uri = github_page + forkName + ".git";
+                    System.out.println("Cloning repo from github: " + forkName + " to " + testCasesDir);
+
+                    jgitUtility.cloneRepo(uri, localSourceCodeDirPath, branchName);
+                    if (forkName.contains("Marlin")) {
+                        try {
+                            FileUtils.deleteDirectory(new File(localSourceCodeDirPath + "ArduinoAddons"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
 
                 /***  get origin diff github page  ***/
                 ParseHtml parseHtml = new ParseHtml(max_numberOfCut, numberOfBiggestClusters, analysisDir, publicToken);
-//                String diffPageUrl = parseHtml.getDiffPageUrl(localSourceCodeDirPath, forkName, timeWindow);
-////            String diffPageUrl = "https://github.com/cruwaller/Marlin/compare/max318xx_dev...MarlinFirmware:1.1.x";
-////            String diffPageUrl = "https://github.com/MarlinFirmware/Marlin/compare/1.1.x...cruwaller:max318xx_dev";
-//                System.out.println(diffPageUrl);
-//
-//
-//                /*** hack github page   ***/
-//                parseHtml.getOriginalDiffPage(diffPageUrl, localSourceCodeDirPath, forkName);
+                String diffPageUrl = parseHtml.getDiffPageUrl(localSourceCodeDirPath, forkName, timeWindow);
+//            String diffPageUrl = "https://github.com/cruwaller/Marlin/compare/max318xx_dev...MarlinFirmware:1.1.x";
+//            String diffPageUrl = "https://github.com/MarlinFirmware/Marlin/compare/1.1.x...cruwaller:max318xx_dev";
+                System.out.println(diffPageUrl);
+
+
+                /*** hack github page   ***/
+                parseHtml.getOriginalDiffPage(diffPageUrl, localSourceCodeDirPath, forkName);
 
 
                 /*** start clustering code  ***/
@@ -137,4 +137,4 @@ public class INFOX_main {
     }
 
 
-//}
+}
